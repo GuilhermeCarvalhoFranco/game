507ca221f793cff14d57ca9b5b2d47255f50b569
GuilhermeCarvalhoFranco <guilherme.carvalho@estudante.ifgoiano.edu.br>
2022-06-20 15:41:00 -0300

    background e imersão
    foi adicionado o fundo do mapa e a imersão do usuário(escondendo, barra de notificação, barra de navegação)

--- app/src/main/java/com/example/gameapp/MainActivity.java
+++ app/src/main/java/com/example/gameapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.gameapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Fazendo um teste de commit
+
+        layoutLandscape();
+    }
+
+    public void layoutLandscape(){
+        int requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+
+        setRequestedOrientation(requestedOrientation);
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
