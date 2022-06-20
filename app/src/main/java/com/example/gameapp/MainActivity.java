c32ad0cea61f5b6f0c647f7911f37c26ca2b5b71
GuilhermeCarvalhoFranco <guilherme.carvalho@estudante.ifgoiano.edu.br>
2022-06-20 19:05:39 -0300

    Música tema de combate incluida
    foi incluida a musica tema de combate

--- app/src/main/java/com/example/gameapp/MainActivity.java
+++ app/src/main/java/com/example/gameapp/MainActivity.java
@@ -4,15 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //private Handler handler;
+
+    MediaPlayer mp;
+
+    boolean isPlaying = false;
+    boolean wasStarted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //handler = new Handler();
 
         layoutLandscape();
     }
@@ -25,4 +35,28 @@ public class MainActivity extends AppCompatActivity {
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
+
+    public void startGame(View v){
+        wasStarted = true;
+
+        themeSoundtrack();
+    }
+
+    public void themeSoundtrack(){
+        mp = MediaPlayer.create(this, R.raw.theme);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(wasStarted){
+                    isPlaying = mp.isPlaying();
+
+                    if (!isPlaying){
+                        mp.start();
+                    }
+                }
+            }
+        }).start();
+    }
+
 }
